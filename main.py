778de9c650b74350cf5e0b32bda30b37765c7dc9
Charles Dawson <charles.dwsn@gmail.com>
2022-08-01 11:03:27 -0600

    Trying tanh mixing

--- main.py
+++ main.py
@@ -10,41 +10,61 @@ from tqdm import tqdm
 
 import os
 import sys
-sys.path.append('systems')
-sys.path.append('configs')
-sys.path.append('models')
+
+sys.path.append("systems")
+sys.path.append("configs")
+sys.path.append("models")
 import argparse
 
 np.random.seed(1024)
 
 parser = argparse.ArgumentParser(description="")
-parser.add_argument('--task', type=str,
-                        default='CAR', help='Name of the model.')
-parser.add_argument('--no_cuda', dest='use_cuda', action='store_false', help='Disable cuda.')
+parser.add_argument("--task", type=str, default="CAR", help="Name of the model.")
+parser.add_argument(
+    "--no_cuda", dest="use_cuda", action="store_false", help="Disable cuda."
+)
 parser.set_defaults(use_cuda=True)
-parser.add_argument('--bs', type=int, default=1024, help='Batch size.')
-parser.add_argument('--num_train', type=int, default=131072, help='Number of samples for training.') # 4096 * 32
-parser.add_argument('--num_test', type=int, default=32768, help='Number of samples for testing.') # 1024 * 32
-parser.add_argument('--lr', dest='learning_rate', type=float, default=0.001, help='Base learning rate.')
-parser.add_argument('--epochs', type=int, default=15, help='Number of training epochs.')
-parser.add_argument('--lr_step', type=int, default=5, help='')
-parser.add_argument('--lambda', type=float, dest='_lambda', default=0.5, help='Convergence rate: lambda')
-parser.add_argument('--w_ub', type=float, default=10, help='Upper bound of the eigenvalue of the dual metric.')
-parser.add_argument('--w_lb', type=float, default=0.1, help='Lower bound of the eigenvalue of the dual metric.')
-parser.add_argument('--log', type=str, help='Path to a directory for storing the log.')
+parser.add_argument("--bs", type=int, default=1024, help="Batch size.")
+parser.add_argument(
+    "--num_train", type=int, default=131072, help="Number of samples for training."
+)  # 4096 * 32
+parser.add_argument(
+    "--num_test", type=int, default=32768, help="Number of samples for testing."
+)  # 1024 * 32
+parser.add_argument(
+    "--lr", dest="learning_rate", type=float, default=0.001, help="Base learning rate."
+)
+parser.add_argument("--epochs", type=int, default=50, help="Number of training epochs.")
+parser.add_argument("--lr_step", type=int, default=10, help="")
+parser.add_argument(
+    "--lambda", type=float, dest="_lambda", default=0.5, help="Convergence rate: lambda"
+)
+parser.add_argument(
+    "--w_ub",
+    type=float,
+    default=10,
+    help="Upper bound of the eigenvalue of the dual metric.",
+)
+parser.add_argument(
+    "--w_lb",
+    type=float,
+    default=0.1,
+    help="Lower bound of the eigenvalue of the dual metric.",
+)
+parser.add_argument("--log", type=str, help="Path to a directory for storing the log.")
 
 args = parser.parse_args()
 
-os.system('cp *.py '+args.log)
-os.system('cp -r models/ '+args.log)
-os.system('cp -r configs/ '+args.log)
-os.system('cp -r systems/ '+args.log)
+os.system("cp *.py " + args.log)
+os.system("cp -r models/ " + args.log)
+os.system("cp -r configs/ " + args.log)
+os.system("cp -r systems/ " + args.log)
 writer = SummaryWriter(args.log + "/tb")
 global_steps = 0
 
 epsilon = args._lambda * 0.1
 
-config = importlib.import_module('config_'+args.task)
+config = importlib.import_module("config_" + args.task)
 X_MIN = config.X_MIN
 X_MAX = config.X_MAX
 U_MIN = config.UREF_MIN
@@ -52,33 +72,38 @@ U_MAX = config.UREF_MAX
 XE_MIN = config.XE_MIN
 XE_MAX = config.XE_MAX
 
-system = importlib.import_module('system_'+args.task)
+system = importlib.import_module("system_" + args.task)
 f_func = system.f_func
 B_func = system.B_func
 num_dim_x = system.num_dim_x
 num_dim_control = system.num_dim_control
-if hasattr(system, 'Bbot_func'):
+if hasattr(system, "Bbot_func"):
     Bbot_func = system.Bbot_func
 
-model = importlib.import_module('model_'+args.task)
+model = importlib.import_module("model_" + args.task)
 get_model = model.get_model
 INVERSE_METRIC = model.INVERSE_METRIC
 
-model_W, model_Wbot, model_u_w1, model_u_w2, W_func, u_func = get_model(num_dim_x, num_dim_control, w_lb=args.w_lb, use_cuda=args.use_cuda)
+model_W, model_Wbot, model_u_w1, W_func, u_func = get_model(
+    num_dim_x, num_dim_control, w_lb=args.w_lb, use_cuda=args.use_cuda
+)
 
 # constructing datasets
 def sample_xef():
-    return (X_MAX-X_MIN) * np.random.rand(num_dim_x, 1) + X_MIN
+    return (X_MAX - X_MIN) * np.random.rand(num_dim_x, 1) + X_MIN
+
 
 def sample_x(xref):
-    xe = (XE_MAX-XE_MIN) * np.random.rand(num_dim_x, 1) + XE_MIN
+    xe = (XE_MAX - XE_MIN) * np.random.rand(num_dim_x, 1) + XE_MIN
     x = xref + xe
-    x[x>X_MAX] = X_MAX[x>X_MAX]
-    x[x<X_MIN] = X_MIN[x<X_MIN]
+    x[x > X_MAX] = X_MAX[x > X_MAX]
+    x[x < X_MIN] = X_MIN[x < X_MIN]
     return x
 
+
 def sample_uref():
-    return (U_MAX-U_MIN) * np.random.rand(num_dim_control, 1) + U_MIN
+    return (U_MAX - U_MIN) * np.random.rand(num_dim_control, 1) + U_MIN
+
 
 def sample_full():
     xref = sample_xef()
@@ -86,18 +111,26 @@ def sample_full():
     x = sample_x(xref)
     return (x, xref, uref)
 
+
 X_tr = [sample_full() for _ in range(args.num_train)]
 X_te = [sample_full() for _ in range(args.num_test)]
 
-if 'Bbot_func' not in locals():
-    def Bbot_func(x): # columns of Bbot forms a basis of the null space of B^T
+if "Bbot_func" not in locals():
+
+    def Bbot_func(x):  # columns of Bbot forms a basis of the null space of B^T
         bs = x.shape[0]
-        Bbot = torch.cat((torch.eye(num_dim_x-num_dim_control, num_dim_x-num_dim_control),
-            torch.zeros(num_dim_control, num_dim_x-num_dim_control)), dim=0)
+        Bbot = torch.cat(
+            (
+                torch.eye(num_dim_x - num_dim_control, num_dim_x - num_dim_control),
+                torch.zeros(num_dim_control, num_dim_x - num_dim_control),
+            ),
+            dim=0,
+        )
         if args.use_cuda:
             Bbot = Bbot.cuda()
         Bbot.unsqueeze(0)
         return Bbot.repeat(bs, 1, 1)
+
 
 def Jacobian_Matrix(M, x):
     # NOTE that this function assume that data are independent of each other
@@ -114,9 +147,10 @@ def Jacobian_Matrix(M, x):
             J[:, i, j, :] = grad(M[:, i, j].sum(), x, create_graph=True)[0].squeeze(-1)
     return J
 
+
 def Jacobian(f, x):
     # NOTE that this function assume that data are independent of each other
-    f = f + 0. * x.sum() # to avoid the case that f is independent of x
+    f = f + 0.0 * x.sum()  # to avoid the case that f is independent of x
     # f: B x m x 1
     # x: B x n x 1
     # ret: B x m x n
@@ -128,6 +162,7 @@ def Jacobian(f, x):
         J[:, i, :] = grad(f[:, i, 0].sum(), x, create_graph=True)[0].squeeze(-1)
     return J
 
+
 def weighted_gradients(W, v, x, detach=False):
     # v, x: bs x n x 1
     # DWDx: bs x n x n x n
@@ -138,19 +173,23 @@ def weighted_gradients(W, v, x, detach=False):
     else:
         return (Jacobian_Matrix(W, x) * v.view(bs, 1, 1, -1)).sum(dim=3)
 
+
 K = 1024
+
+
 def loss_pos_matrix_random_sampling(A):
     # A: bs x d x d
     # z: K x d
     z = torch.randn(K, A.size(-1)).cuda()
     z = z / z.norm(dim=1, keepdim=True)
-    zTAz = (z.matmul(A) * z.view(1,K,-1)).sum(dim=2).view(-1)
+    zTAz = (z.matmul(A) * z.view(1, K, -1)).sum(dim=2).view(-1)
     negative_index = zTAz.detach().cpu().numpy() < 0
-    if negative_index.sum()>0:
+    if negative_index.sum() > 0:
         negative_zTAz = zTAz[negative_index]
         return -1.0 * (negative_zTAz.mean())
     else:
-        return torch.tensor(0.).type(z.type()).requires_grad_()
+        return torch.tensor(0.0).type(z.type()).requires_grad_()
+
 
 def loss_pos_matrix_eigen_values(A):
     # A: bs x d x d
@@ -158,6 +197,7 @@ def loss_pos_matrix_eigen_values(A):
     negative_index = eigv.detach().cpu().numpy() < 0
     negative_eigv = eigv[negative_index]
     return negative_eigv.norm()
+
 
 def forward(x, xref, uref, _lambda, verbose=False, acc=False, detach=False):
     # x: bs x n x 1
@@ -173,51 +213,105 @@ def forward(x, xref, uref, _lambda, verbose=False, acc=False, detach=False):
     DfDx = Jacobian(f, x)
     DBDx = torch.zeros(bs, num_dim_x, num_dim_x, num_dim_control).type(x.type())
     for i in range(num_dim_control):
-        DBDx[:,:,:,i] = Jacobian(B[:,:,i].unsqueeze(-1), x)
+        DBDx[:, :, :, i] = Jacobian(B[:, :, i].unsqueeze(-1), x)
 
     _Bbot = Bbot_func(x)
-    u = u_func(x, x - xref, uref) # u: bs x m x 1 # TODO: x - xref
+    u = u_func(x, x - xref, uref)  # u: bs x m x 1 # TODO: x - xref
     K = Jacobian(u, x)
 
-    A = DfDx + sum([u[:, i, 0].unsqueeze(-1).unsqueeze(-1) * DBDx[:, :, :, i] for i in range(num_dim_control)])
+    A = DfDx + sum(
+        [
+            u[:, i, 0].unsqueeze(-1).unsqueeze(-1) * DBDx[:, :, :, i]
+            for i in range(num_dim_control)
+        ]
+    )
     dot_x = f + B.matmul(u)
-    dot_M = weighted_gradients(M, dot_x, x, detach=detach) # DMDt
-    dot_W = weighted_gradients(W, dot_x, x, detach=detach) # DWDt
+    dot_M = weighted_gradients(M, dot_x, x, detach=detach)  # DMDt
+    dot_W = weighted_gradients(W, dot_x, x, detach=detach)  # DWDt
     if detach:
-        Contraction = dot_M + (A + B.matmul(K)).transpose(1,2).matmul(M.detach()) + M.detach().matmul(A + B.matmul(K)) + 2 * _lambda * M.detach()
+        Contraction = (
+            dot_M
+            + (A + B.matmul(K)).transpose(1, 2).matmul(M.detach())
+            + M.detach().matmul(A + B.matmul(K))
+            + 2 * _lambda * M.detach()
+        )
     else:
-        Contraction = dot_M + (A + B.matmul(K)).transpose(1,2).matmul(M) + M.matmul(A + B.matmul(K)) + 2 * _lambda * M
+        Contraction = (
+            dot_M
+            + (A + B.matmul(K)).transpose(1, 2).matmul(M)
+            + M.matmul(A + B.matmul(K))
+            + 2 * _lambda * M
+        )
 
     # C1
-    C1_inner = - weighted_gradients(W, f, x) + DfDx.matmul(W) + W.matmul(DfDx.transpose(1,2)) + 2 * _lambda * W
-    C1_LHS_1 = _Bbot.transpose(1,2).matmul(C1_inner).matmul(_Bbot) # this has to be a negative definite matrix
+    C1_inner = (
+        -weighted_gradients(W, f, x)
+        + DfDx.matmul(W)
+        + W.matmul(DfDx.transpose(1, 2))
+        + 2 * _lambda * W
+    )
+    C1_LHS_1 = (
+        _Bbot.transpose(1, 2).matmul(C1_inner).matmul(_Bbot)
+    )  # this has to be a negative definite matrix
 
     # C2
     C2_inners = []
     C2s = []
     for j in range(num_dim_control):
-        C2_inner = weighted_gradients(W, B[:,:,j].unsqueeze(-1), x) - (DBDx[:,:,:,j].matmul(W) + W.matmul(DBDx[:,:,:,j].transpose(1,2)))
-        C2 = _Bbot.transpose(1,2).matmul(C2_inner).matmul(_Bbot)
+        C2_inner = weighted_gradients(W, B[:, :, j].unsqueeze(-1), x) - (
+            DBDx[:, :, :, j].matmul(W) + W.matmul(DBDx[:, :, :, j].transpose(1, 2))
+        )
+        C2 = _Bbot.transpose(1, 2).matmul(C2_inner).matmul(_Bbot)
         C2_inners.append(C2_inner)
         C2s.append(C2)
 
     loss = 0
-    loss += loss_pos_matrix_random_sampling(-Contraction - epsilon * torch.eye(Contraction.shape[-1]).unsqueeze(0).type(x.type()))
-    loss += loss_pos_matrix_random_sampling(-C1_LHS_1 - epsilon * torch.eye(C1_LHS_1.shape[-1]).unsqueeze(0).type(x.type()))
-    loss += loss_pos_matrix_random_sampling(args.w_ub * torch.eye(W.shape[-1]).unsqueeze(0).type(x.type()) - W)
+    loss += loss_pos_matrix_random_sampling(
+        -Contraction
+        - epsilon * torch.eye(Contraction.shape[-1]).unsqueeze(0).type(x.type())
+    )
+    loss += loss_pos_matrix_random_sampling(
+        -C1_LHS_1 - epsilon * torch.eye(C1_LHS_1.shape[-1]).unsqueeze(0).type(x.type())
+    )
+    loss += loss_pos_matrix_random_sampling(
+        args.w_ub * torch.eye(W.shape[-1]).unsqueeze(0).type(x.type()) - W
+    )
     # loss += loss_pos_matrix_random_sampling(W - args.w_lb * torch.eye(W.shape[-1]).unsqueeze(0).type(x.type()))  # Make sure W is positive definite
-    loss += 1. * sum([1.*(C2**2).reshape(bs,-1).sum(dim=1).mean() for C2 in C2s])
+    loss += 1.0 * sum([1.0 * (C2**2).reshape(bs, -1).sum(dim=1).mean() for C2 in C2s])
 
     if verbose:
-        print(torch.symeig(Contraction)[0].min(dim=1)[0].mean(), torch.symeig(Contraction)[0].max(dim=1)[0].mean(), torch.symeig(Contraction)[0].mean())
+        print(
+            torch.symeig(Contraction)[0].min(dim=1)[0].mean(),
+            torch.symeig(Contraction)[0].max(dim=1)[0].mean(),
+            torch.symeig(Contraction)[0].mean(),
+        )
     if acc:
-        return loss, ((torch.symeig(Contraction)[0]>=0).sum(dim=1)==0).cpu().detach().numpy(), ((torch.symeig(C1_LHS_1)[0]>=0).sum(dim=1)==0).cpu().detach().numpy(), sum([1.*(C2**2).reshape(bs,-1).sum(dim=1).mean() for C2 in C2s]).item()
+        return (
+            loss,
+            ((torch.symeig(Contraction)[0] >= 0).sum(dim=1) == 0)
+            .cpu()
+            .detach()
+            .numpy(),
+            ((torch.symeig(C1_LHS_1)[0] >= 0).sum(dim=1) == 0).cpu().detach().numpy(),
+            sum(
+                [1.0 * (C2**2).reshape(bs, -1).sum(dim=1).mean() for C2 in C2s]
+            ).item(),
+        )
     else:
         return loss, None, None, None
 
-optimizer = torch.optim.Adam(list(model_W.parameters()) + list(model_Wbot.parameters()) + list(model_u_w1.parameters()) + list(model_u_w2.parameters()), lr=args.learning_rate)
 
-def trainval(X, bs=args.bs, train=True, _lambda=args._lambda, acc=False, detach=False): # trainval a set of x
+optimizer = torch.optim.Adam(
+    list(model_W.parameters())
+    + list(model_Wbot.parameters())
+    + list(model_u_w1.parameters()),
+    lr=args.learning_rate,
+)
+
+
+def trainval(
+    X, bs=args.bs, train=True, _lambda=args._lambda, acc=False, detach=False
+):  # trainval a set of x
     # torch.autograd.set_detect_anomaly(True)
 
     if train:
@@ -236,8 +330,10 @@ def trainval(X, bs=args.bs, train=True, _lambda=args._lambda, acc=False, detach=
         _iter = range(len(X) // bs)
     for b in _iter:
         start = time.time()
-        x = []; xref = []; uref = [];
-        for id in indices[b*bs:(b+1)*bs]:
+        x = []
+        xref = []
+        uref = []
+        for id in indices[b * bs : (b + 1) * bs]:
             if args.use_cuda:
                 x.append(torch.from_numpy(X[id][0]).float().cuda())
                 xref.append(torch.from_numpy(X[id][1]).float().cuda())
@@ -252,7 +348,15 @@ def trainval(X, bs=args.bs, train=True, _lambda=args._lambda, acc=False, detach=
 
         start = time.time()
 
-        loss, p1, p2, l3 = forward(x, xref, uref, _lambda=_lambda, verbose=False if not train else False, acc=acc, detach=detach)
+        loss, p1, p2, l3 = forward(
+            x,
+            xref,
+            uref,
+            _lambda=_lambda,
+            verbose=False if not train else False,
+            acc=acc,
+            detach=detach,
+        )
 
         start = time.time()
         if train:
@@ -266,35 +370,89 @@ def trainval(X, bs=args.bs, train=True, _lambda=args._lambda, acc=False, detach=
             total_p1 += p1.sum()
             total_p2 += p2.sum()
             total_l3 += l3 * x.shape[0]
-    return total_loss / len(X), total_p1 / len(X), total_p2 / len(X), total_l3/ len(X)
+    return total_loss / len(X), total_p1 / len(X), total_p2 / len(X), total_l3 / len(X)
 
 
 best_acc = 0
+
 
 def adjust_learning_rate(optimizer, epoch):
     """Sets the learning rate to the initial LR decayed by every args.lr_step epochs"""
     lr = args.learning_rate * (0.1 ** (epoch // args.lr_step))
     for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
+        param_group["lr"] = lr
 
+
+# # DEBUG
+# import matplotlib.pyplot as plt
+
+# x = torch.stack([torch.from_numpy(x[0]) for x in X_te[:10000]]).float()
+# x[:, 2] = 0.0
+# x[:, 3] = 1.0
+# x = x.requires_grad_()
+# xref = torch.stack([torch.from_numpy(x[1]) for x in X_te]).float()
+# uref = torch.stack([torch.from_numpy(x[2]) for x in X_te]).float()
+
+# N = 5
+# mixing_ratios = torch.linspace(0.0, 1.0, N)
+# fix, axs = plt.subplots(1, N)
+
+# for i in range(N):
+#     mixing = mixing_ratios[i]
+#     u_func.set_mixing(mixing)
+
+#     u = u_func(x, x - xref[0], uref[0])  # u: bs x m x 1 # TODO: x - xref
+#     K = Jacobian(u, x)
+
+#     axs[i].scatter(x[:, 0].detach(), x[:, 1].detach(), c=K[:, 0, 0].detach())
+#     axs[i].set_title(f"K[0, 0] ({1 - mixing} tanh + {mixing} hardtanh)")
+#     axs[i].set_xlabel("x")
+#     axs[i].set_ylabel("y")
+
+# plt.show()
+
+reset = 0
 for epoch in range(args.epochs):
-    adjust_learning_rate(optimizer, epoch)
-    loss, _, _, _ = trainval(X_tr, train=True, _lambda=args._lambda, acc=False, detach=True if epoch < args.lr_step else False)
+    # Start mixing in hardtanh after some initial training
+    if epoch >= 20:
+        mixing = min((epoch - 20) / 10, 1.0)
+        print(f"** Mixing {1 - mixing} Tanh + {mixing} Hardtanh **")
+        W_func.set_mixing(mixing)
+        u_func.set_mixing(mixing)
+        reset = 20
+
+    adjust_learning_rate(optimizer, epoch - reset)
+    loss, _, _, _ = trainval(
+        X_tr,
+        train=True,
+        _lambda=args._lambda,
+        acc=False,
+        detach=True if epoch - reset < args.lr_step else False,
+    )
     print("Training loss: ", loss)
-    loss, p1, p2, l3 = trainval(X_te, train=False, _lambda=0., acc=True, detach=False)
-    print("Epoch %d: Testing loss/p1/p2/l3: "%epoch, loss, p1, p2, l3)
+    loss, p1, p2, l3 = trainval(X_te, train=False, _lambda=0.0, acc=True, detach=False)
+    print("Epoch %d: Testing loss/p1/p2/l3: " % epoch, loss, p1, p2, l3)
 
     writer.add_scalar("Loss", loss, epoch)
     writer.add_scalar("% of pts with max eig Q < 0", p1, epoch)
     writer.add_scalar("% of pts with max eig C1_LHS < 0", p2, epoch)
     writer.add_scalar("mean C2^2", l3, epoch)
 
-    if p1+p2 >= best_acc:
+    if p1 + p2 >= best_acc:
         best_acc = p1 + p2
-        filename = args.log+'/model_best.pth.tar'
-        filename_controller = args.log+'/controller_best.pth.tar'
-        filename_metric = args.log+'/metric_best.pth.tar'
-        torch.save({'args':args, 'precs':(loss, p1, p2), 'model_W': model_W.state_dict(), 'model_Wbot': model_Wbot.state_dict(), 'model_u_w1': model_u_w1.state_dict(), 'model_u_w2': model_u_w2.state_dict()}, filename)
+        filename = args.log + "/model_best.pth.tar"
+        filename_controller = args.log + "/controller_best.pth.tar"
+        filename_metric = args.log + "/metric_best.pth.tar"
+        torch.save(
+            {
+                "args": args,
+                "precs": (loss, p1, p2),
+                "model_W": model_W.state_dict(),
+                "model_Wbot": model_Wbot.state_dict(),
+                "model_u_w1": model_u_w1.state_dict(),
+            },
+            filename,
+        )
         torch.save(u_func, filename_controller)
         torch.save(W_func, filename_metric)
 
--- models/model_CAR.py
+++ models/model_CAR.py
@@ -6,14 +6,29 @@ import numpy as np
 effective_dim_start = 2
 effective_dim_end = 4
 
+INVERSE_METRIC = False
+
+
+class MixedTanh(nn.Module):
+    def __init__(self):
+        super(MixedTanh, self).__init__()
+        self.mixing = 0.0  # start as all tanh
+
+    def set_mixing(self, mixing):
+        self.mixing = mixing
+
+    def forward(self, x):
+        tanh = torch.nn.functional.tanh(x)
+        hardtanh = torch.nn.functional.hardtanh(x) 
+        return (1.0 - self.mixing) * tanh + self.mixing * hardtanh
+
 
 class U_FUNC(nn.Module):
     """docstring for U_FUNC."""
 
-    def __init__(self, model_u_w1, model_u_w2, num_dim_x, num_dim_control):
+    def __init__(self, model_u_w1, num_dim_x, num_dim_control):
         super(U_FUNC, self).__init__()
         self.model_u_w1 = model_u_w1
-        self.model_u_w2 = model_u_w2
         self.num_dim_x = num_dim_x
         self.num_dim_control = num_dim_control
 
@@ -31,59 +46,107 @@ class U_FUNC(nn.Module):
         u = w1_xe - w1_x0 + uref
         return u
 
+    def convert_to_hardtanh(self):
+        for i, layer in enumerate(self.model_u_w1):
+            if layer._get_name() == "Tanh":
+                self.model_u_w1[i] = torch.nn.Hardtanh()
+
+    def set_mixing(self, mixing):
+        for i, layer in enumerate(self.model_u_w1):
+            if layer._get_name() == "MixedTanh":
+                self.model_u_w1[i].set_mixing(mixing)
+
+
+class W_FUNC(nn.Module):
+    """docstring for W_FUNC."""
+
+    def __init__(self, model_W, model_Wbot, num_dim_x, num_dim_control, w_lb):
+        super(W_FUNC, self).__init__()
+        self.model_W = model_W
+        self.model_Wbot = model_Wbot
+        self.w_lb = w_lb
+        self.num_dim_x = num_dim_x
+        self.num_dim_control = num_dim_control
+
+    def forward(self, x):
+        bs = x.shape[0]
+        x = x.squeeze(-1)
+
+        W = self.model_W(x[:, effective_dim_start:effective_dim_end]).view(
+            bs, self.num_dim_x, self.num_dim_x
+        )
+        Wbot = self.model_Wbot(torch.ones(bs, 1).type(x.type())).view(
+            bs,
+            self.num_dim_x - self.num_dim_control,
+            self.num_dim_x - self.num_dim_control,
+        )
+        W[
+            :,
+            0 : self.num_dim_x - self.num_dim_control,
+            0 : self.num_dim_x - self.num_dim_control,
+        ] = Wbot
+        W[
+            :,
+            self.num_dim_x - self.num_dim_control : :,
+            0 : self.num_dim_x - self.num_dim_control,
+        ] = 0
+
+        # W = self.model_W(x[:, effective_dim_start:effective_dim_end]).view(bs, self.num_dim_x, self.num_dim_x)
+
+        W = W.transpose(1, 2).matmul(W)
+        W = W + self.w_lb * torch.eye(self.num_dim_x).view(
+            1, self.num_dim_x, self.num_dim_x
+        ).type(x.type())
+        return W
+
+    def convert_to_hardtanh(self):
+        for i, layer in enumerate(self.model_W):
+            if layer._get_name() == "Tanh":
+                self.model_W[i] = torch.nn.Hardtanh()
+
+        for i, layer in enumerate(self.model_Wbot):
+            if layer._get_name() == "Tanh":
+                self.model_Wbot[i] = torch.nn.Hardtanh()
+
+    def set_mixing(self, mixing):
+        for i, layer in enumerate(self.model_W):
+            if layer._get_name() == "MixedTanh":
+                self.model_W[i].set_mixing(mixing)
+
+        for i, layer in enumerate(self.model_Wbot):
+            if layer._get_name() == "MixedTanh":
+                self.model_Wbot[i].set_mixing(mixing)
+
 
 def get_model(num_dim_x, num_dim_control, w_lb, use_cuda=False):
     model_Wbot = torch.nn.Sequential(
         torch.nn.Linear(1, 128, bias=True),
-        torch.nn.Tanh(),
+        MixedTanh(),
         torch.nn.Linear(128, (num_dim_x - num_dim_control) ** 2, bias=False),
     )
 
     dim = effective_dim_end - effective_dim_start
     model_W = torch.nn.Sequential(
         torch.nn.Linear(dim, 128, bias=True),
-        torch.nn.Tanh(),
+        MixedTanh(),
         torch.nn.Linear(128, num_dim_x * num_dim_x, bias=False),
     )
 
-    c = 3 * num_dim_x
     model_u_w1 = torch.nn.Sequential(
-        torch.nn.Linear(2 * num_dim_x, 128, bias=True),
-        torch.nn.Tanh(),
-        torch.nn.Linear(128, num_dim_control, bias=True),
-    )
-
-    model_u_w2 = torch.nn.Sequential(
-        torch.nn.Linear(2 * num_dim_x, 128, bias=True),
-        torch.nn.Tanh(),
-        torch.nn.Linear(128, num_dim_control, bias=True),
+        # torch.nn.Linear(2 * num_dim_x, num_dim_control, bias=True),
+        torch.nn.Linear(2 * num_dim_x, 64, bias=False),
+        MixedTanh(),
+        torch.nn.Linear(64, 64, bias=False),
+        MixedTanh(),
+        torch.nn.Linear(64, num_dim_control, bias=False),
     )
 
     if use_cuda:
         model_W = model_W.cuda()
         model_Wbot = model_Wbot.cuda()
         model_u_w1 = model_u_w1.cuda()
-        model_u_w2 = model_u_w2.cuda()
 
-    def W_func(x):
-        bs = x.shape[0]
-        x = x.squeeze(-1)
+    u_func = U_FUNC(model_u_w1, num_dim_x, num_dim_control)
+    W_func = W_FUNC(model_W, model_Wbot, num_dim_x, num_dim_control, w_lb)
 
-        W = model_W(x[:, effective_dim_start:effective_dim_end]).view(
-            bs, num_dim_x, num_dim_x
-        )
-        Wbot = model_Wbot(torch.ones(bs, 1).type(x.type())).view(
-            bs, num_dim_x - num_dim_control, num_dim_x - num_dim_control
-        )
-        W[:, 0 : num_dim_x - num_dim_control, 0 : num_dim_x - num_dim_control] = Wbot
-        W[:, num_dim_x - num_dim_control : :, 0 : num_dim_x - num_dim_control] = 0
-
-        # W = model_W(x[:, effective_dim_start:effective_dim_end]).view(bs, num_dim_x, num_dim_x)
-
-        W = W.transpose(1, 2).matmul(W)
-        W = W + w_lb * torch.eye(num_dim_x).view(1, num_dim_x, num_dim_x).type(x.type())
-        return W
-
-    u_func = U_FUNC(model_u_w1, model_u_w2, num_dim_x, num_dim_control)
-
-    return model_W, model_Wbot, model_u_w1, model_u_w2, W_func, u_func
+    return model_W, model_Wbot, model_u_w1, W_func, u_func
